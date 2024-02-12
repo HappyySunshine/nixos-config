67f5c1b1458e6af5cd5e39cada6c888d7951ed31
Happy sunshine <maxthedog1200@gmail.com>
2024-02-12 10:41:20 -0300

    yay :D

--- .trash/colors/Banana-main.lua
+++
@@ -1,4 +0,0 @@
-package.loaded['banana.palette'] = nil
-package.loaded['banana.theme'] = nil
-
-require('banana').colorscheme()
--- .trash/macros.lua
+++
@@ -1,2 +0,0 @@
--- vim.keymap.set('n', '<leader>st', '<cmd>colorscheme Banana-main<CR>', {silent=true})
--- vim.keymap.set('n', '<leader>ca', '<cmd>!na:sm -f elf hello.s  -o hello.o && ld -m elf_i386 -o hello hello.o && ./hello<CR>', {desc="compile assembly"})
